3484c6d55cfe99904561b2d8bfed470297a5c864
all_caaash <une066@gmail.com>
2023-11-25 18:51:12 +0500

    create new project lr3

---
+++ src/lr3/example19_01.java
@@ -0,0 +1,13 @@
+package lr3;
+
+import java.util.Scanner;
+
+public class example19_01 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
