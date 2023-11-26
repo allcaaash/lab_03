0fb29eaa1d4453a7bdc0062e8bfd373700c056f0
all_caaash <une066@gmail.com>
2023-11-26 11:32:10 +0500

    _04

---
+++ src/lr3/example19_04_for.java
@@ -0,0 +1,36 @@
+package lr3;
+
+import java.util.Scanner;
+
+public class example19_04_for {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = 0;
+        int secondNum = 0;
+
+        System.out.println("Enter two whole integers: ");
+        firstNum = scanner.nextInt();
+        secondNum = scanner.nextInt();
+
+        if(firstNum == secondNum){
+
+            System.out.println("The numbers are equal.");
+
+            return;
+        }
+
+        if (firstNum > secondNum) {
+
+            int temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+        }
+
+        for (int it = firstNum; it <= secondNum; it++){
+
+            System.out.print(it + " ");
+        }
+    }
+}
---
+++ src/lr3/example19_04_while.java
@@ -0,0 +1,38 @@
+package lr3;
+
+import java.util.Scanner;
+
+public class example19_04_while {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int firstNum = 0;
+        int secondNum = 0;
+
+        System.out.println("Enter two whole integers: ");
+        firstNum = scanner.nextInt();
+        secondNum = scanner.nextInt();
+
+        if(firstNum == secondNum){
+
+            System.out.println("The numbers are equal.");
+
+            return;
+        }
+
+        if (firstNum > secondNum) {
+
+            int temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+        }
+
+        int it = firstNum;
+        while(it <= secondNum){
+
+            System.out.print(it + " ");
+            it++;
+        }
+    }
+}
